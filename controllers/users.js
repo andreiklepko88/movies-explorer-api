6fb19af8014c0121dac3194ac2e566eb2a220ac3
andreiklepko88 <118067419+andreiklepko88@users.noreply.github.com>
2023-08-20 16:49:03 +0300

    changed jwt

--- controllers/users.js
+++ controllers/users.js
@@ -68,9 +68,8 @@ const login = (req, res, next) => {
           const token = jwt.sign({ id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
           res.cookie('jwt', token, {
             maxAge: 3600000 * 24 * 7,
-            httpOnly: true,
-            sameSite: 'none',
             secure: true,
+            sameSite: 'none',
           });
           return res.status(OK_CODE).send({ token });
         });
