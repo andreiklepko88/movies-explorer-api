853be7617412df881919e4753cb7c96938479ba0
andreiklepko88 <118067419+andreiklepko88@users.noreply.github.com>
2023-08-26 21:57:09 +0300

    Changed director.length

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -32,7 +32,7 @@ const validateDeleteMovie = celebrate({
 const validateCreateMovie = celebrate({
   body: Joi.object().keys({
     country: Joi.string().required().min(2).max(30),
-    director: Joi.string().required().min(2).max(30),
+    director: Joi.string().required().min(2).max(50),
     duration: Joi.number().required(),
     year: Joi.string().required(),
     description: Joi.string().required(),
