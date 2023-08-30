5872193fac7c2ac8591f4cca0a86316f8c4544bc
andreiklepko88 <118067419+andreiklepko88@users.noreply.github.com>
2023-08-31 00:49:09 +0300

    deploy frontend

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://api.bitfilms-andreikodev.nomoreparties.sbs',
+  origin: 'https://bitfilms-andreikodev.nomoreparties.sbs',
   credentials: true,
 }));
 
