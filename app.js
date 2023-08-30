60966332d7ca93c7c5aeccac27839e7a6bc86313
andreiklepko88 <118067419+andreiklepko88@users.noreply.github.com>
2023-08-30 11:36:20 +0300

    back to local

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://bitfilms-andreikodev.nomoreparties.sbs',
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
 
